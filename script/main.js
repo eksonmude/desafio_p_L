17efd63c2d7d1bb3fecfeb234ecb95c8ff26e71c
eksonezequielm@gmail.com <eksonezequielm@gmail.com>
2024-01-24 19:30:14 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+   const indexManager = {
+      currentPageIndex: 0,
+      fadeBehavior: [['initial', 'rules']],
+   }
+
+   function fadeSection() {
+      const sections = {
+         current: document.querySelector(`.${indexManager.fadeBehavior[indexManager.currentPageIndex][0]}`),
+         previus: document.querySelector(`.${indexManager.fadeBehavior[indexManager.currentPageIndex][1]}`)
+      }
+
+      sections.current.classList.add('reset-opacity')
+
+      setTimeout(() => {
+         sections.current.classList.add('hide')
+         sections.previus.classList.remove('hide')
+      }, 500);
+
+      setTimeout(() => sections.previus.classList.remove('reset-opacity'), 600);
+
+      ++indexManager.currentPageIndex
+   }
+
+   // setup each fader-btn after 3 sec's
+   setTimeout(() => {
+      document.querySelector('#initial-btn').classList.add('show')
+
+      document.querySelectorAll('.fader-btn').forEach(btn => btn.addEventListener('click', fadeSection))
+   },);
+}
